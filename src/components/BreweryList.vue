41924ea0f6e0fd3fefc9bb07d31557b0c817ec28
Kara Alexandra <ardnaxelarak@gmail.com>
2022-12-07 14:37:18 -0800

    Move brewery data to BreweryList component

--- src/components/BreweryList.vue
+++ src/components/BreweryList.vue
@@ -5,10 +5,29 @@ import BreweryListItem from './BreweryListItem.vue'
 export default defineComponent({
   components: {
     BreweryListItem,
-  }
+  },
+  data() {
+    return {
+      breweries: [
+        {
+          name: "MadTree Brewing",
+          type: "regional",
+          street: "3301 Madison Rd",
+          address_2: null,
+          address_3: null,
+          city: "Cincinnati",
+          state: "Ohio",
+          postal_code: "45209-1132",
+          website_url: "http://www.madtreebrewing.com",
+        }
+      ],
+    }
+  },
 })
 </script>
 
 <template>
-  <BreweryListItem />
+  <div v-for="brewery in breweries">
+    <BreweryListItem :brewery="brewery" />
+  </div>
 </template>
--- src/components/BreweryListItem.vue
+++ src/components/BreweryListItem.vue
@@ -2,30 +2,20 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  data() {
-    return {
-      name: "MadTree Brewing",
-      type: "regional",
-      street: "3301 Madison Rd",
-      address_2: null,
-      address_3: null,
-      city: "Cincinnati",
-      state: "Ohio",
-      postal_code: "45209-1132",
-      website_url: "http://www.madtreebrewing.com",
-    }
-  }
+  props: {
+    brewery: Object,
+  },
 })
 </script>
 
 <template>
   <div>
-    <h2>{{ name }}</h2>
-    <h4>{{ type }}</h4>
-    <p>{{ street }}</p>
-    <p v-if="address_2">{{ address_2 }}</p>
-    <p v-if="address_3">{{ address_3 }}</p>
-    <p>{{ city }}, {{ state }} {{ postal_code }}</p>
-    <p v-if="website_url"><a :href="website_url">{{ website_url }}</a></p>
+    <h2>{{ brewery.name }}</h2>
+    <h4>{{ brewery.type }}</h4>
+    <p>{{ brewery.street }}</p>
+    <p v-if="brewery.address_2">{{ brewery.address_2 }}</p>
+    <p v-if="brewery.address_3">{{ brewery.address_3 }}</p>
+    <p>{{ brewery.city }}, {{ brewery.state }} {{ brewery.postal_code }}</p>
+    <p v-if="brewery.website_url"><a :href="website_url">{{ brewery.website_url }}</a></p>
   </div>
 </template>
