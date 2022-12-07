3e9d1096578b59c006e6b6c05cba92c672c6445d
Kara Alexandra <ardnaxelarak@gmail.com>
2022-12-07 14:26:21 -0800

    Add basic brewery listing with static data

---
+++ src/components/BreweryList.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import BreweryListItem from './BreweryListItem.vue'
+
+export default defineComponent({
+  components: {
+    BreweryListItem,
+  }
+})
+</script>
+
+<template>
+  <BreweryListItem />
+</template>
---
+++ src/components/BreweryListItem.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      name: "MadTree Brewing",
+      type: "regional",
+      street: "3301 Madison Rd",
+      address_2: null,
+      address_3: null,
+      city: "Cincinnati",
+      state: "Ohio",
+      postal_code: "45209-1132",
+      website_url: "http://www.madtreebrewing.com",
+    }
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <h4>{{ type }}</h4>
+    <p>{{ street }}</p>
+    <p v-if="address_2">{{ address_2 }}</p>
+    <p v-if="address_3">{{ address_3 }}</p>
+    <p>{{ city }}, {{ state }} {{ postal_code }}</p>
+    <p v-if="website_url"><a :href="website_url">{{ website_url }}</a></p>
+  </div>
+</template>
