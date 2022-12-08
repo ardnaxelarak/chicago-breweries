6375fd326b20f129a804fb8d58c765bea6fdaa0d
Kara Alexandra <ardnaxelarak@gmail.com>
2022-12-07 16:45:19 -0800

    More styling

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,7 @@ import BreweryList from './components/BreweryList.vue'
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+body {
+  color: #F7CB5C;
 }
 </style>
--- src/components/BreweryListItem.vue
+++ src/components/BreweryListItem.vue
@@ -14,25 +14,49 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="card brewery-list-item">
-    <h3 class="brewery-name">{{ brewery.name }}</h3>
-    <p v-if="brewery.brewery_type"><span class="badge bg-secondary">{{ brewery.brewery_type }}</span></p>
-    <p>
-      <span v-if="brewery.street">{{ brewery.street }}<br></span>
-      <span v-if="brewery.address_2">{{ brewery.address_2 }}<br></span>
-      <span v-if="brewery.address_3">{{ brewery.address_3 }}<br></span>
-      <span>{{ brewery.city }}, {{ brewery.state }} {{ brewery.postal_code }}</span>
-    </p>
-    <p v-if="brewery.website_url"><a :href="brewery.website_url">{{ brewery.website_url }}</a></p>
+  <div class="card brewery-list-card">
+    <div class="brewery-list-item">
+      <span class="badge bg-secondary brewery-badge">{{ brewery.brewery_type }}</span>
+      <div class=brewery-details>
+        <span class="brewery-name">{{ brewery.name }}</span>
+        <div class="address">
+          <span v-if="brewery.street">{{ brewery.street }}<br></span>
+          <span v-if="brewery.address_2">{{ brewery.address_2 }}<br></span>
+          <span v-if="brewery.address_3">{{ brewery.address_3 }}<br></span>
+          <span>{{ brewery.city }}, {{ brewery.state }} {{ brewery.postal_code }}</span>
+        </div>
+        <div v-if="brewery.website_url" class="brewery-url">
+          <a :href="brewery.website_url">{{ brewery.website_url }}</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
-.brewery-list-item {
+.brewery-list-card {
   width: 30em;
-  margin: auto;
+  margin: 1em auto;
+}
+.brewery-list-card:hover {
+  background: #CCC;
+  cursor: pointer;
+}
+.brewery-list-item {
+  display: flex;
+}
+.brewery-details {
+  padding: 1em;
 }
 .brewery-name {
-  text-align: center;
+  font-size: 150%;
+  font-weight: bold;
+}
+.brewery-badge {
+  writing-mode: vertical-rl;
+  transform: rotate(180deg);
+}
+.address {
+  margin: 0.5em 1em;
 }
 </style>
